96e51ef1548a8e554b706ce7bcc3da78c8153f5e
Andrii <89638386+Noranmo@users.noreply.github.com>
2024-07-29 20:05:05 +0200

    Fix the error with deployment

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-export function webpack(config, { dev }) {
-	config.module.rules.push({
-		test: /\.pdf$/i,
-		type: 'asset/source',
-	})
-	return config
-}
